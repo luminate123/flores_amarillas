3471027973ff66fc454d7abad5d52ee4f7299d4c
luminate123 <ssenmache8@gmail.com>
2024-09-22 00:44:52 -0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,73 @@
+"use client"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from "@/components/ui/button"
+import { useState, useEffect } from 'react';
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  useEffect(() => {
+    setIsDialogOpen(true);
+  }, []);
+
+  return (
+    <div className="bg-[#D9D9D9] grid  items-center justify-items-center min-h-screen  font-[family-name:var(--font-geist-sans)]">
+      <div>
+        <h1 className="font-semibold text-3xl font-mono text-center">
+          Se que no es mucho y ostentoso pero lo dibuje yo solito
+        </h1>
+      </div>
+      <AlertDialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+        <AlertDialogTrigger asChild>
+          <Button variant="outline" className="bg-amber-300">Leelo</Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle></AlertDialogTitle>
+            <AlertDialogDescription>
+
+              Sé que ayer fue el día de las rosas amarillas, no me gusta que seas espectadora hoy declaro el día de la orquídea blanca. Aunque no lo creas, la orquídea la dibuje yo, puede ser que me haya guiado de una imagen de google, pero la hice yo ehh
+              <br />
+              Así como las orquideas estaremos, juntitos.
+              <br />
+              <div className="font-semibold">
+                Espero que te guste.
+                <br />
+                Te amo con todo mi corazón.
+              </div>
+
+
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogAction className="bg-red-400">Te amo</AlertDialogAction>
+
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+      <Image
+        src="/floresfinal171_1.svg"
+        alt="Flor Amarilla"
+        width={350}
+        height={350}
+      />
+
+      <Button asChild className="mb-8 bg-fuchsia-500">
+        <Link href="/login">Si te gusto?</Link>
+      </Button>
+
+    </div>
+  );
+}
